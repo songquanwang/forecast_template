e13855293b349ba9452659f663f552b2c25335be
宋全旺 <784592656@qq.com>
2019-05-22 18:01:39 +0800

    laglat

--- forecast/recommend/rec/get_weather_address.py
+++ forecast/recommend/rec/get_weather_address.py
@@ -114,5 +114,34 @@ def get_address():
     add_df.to_csv('lntlat_address.csv', index=False)
 
 
+from urllib import parse
+import hashlib
+
+
+def get_urt(address):
+    """
+    获取地址经纬度
+    {"status":0,"result":{"location":{"lng":116.21412109279787,"lat":39.94960819303885},
+    "precise":1,"confidence":80,"comprehension":44,"level":"生活服务"}}
+    :param address:
+    :return:
+    """
+    ak = 'hPOW2xN1TzUTKQUqaLFiehUV0g2ypc36'
+    queryStr = '/geocoder/v2/?address=%s&output=json&ak={}'.format(ak, address)
+    # 对queryStr进行转码，safe内的保留字符不转换
+    encodedStr = parse.quote(queryStr, safe="/:=&?#+!$,;'@()*[]")
+    # 在最后直接追加上yoursk
+    rawStr = encodedStr + 'hPOW2xN1TzUTKQUqaLFiehUV0g2ypc36'
+    # 计算sn
+    sn = (hashlib.md5(parse.quote_plus(rawStr).encode("utf8")).hexdigest())
+    # 由于URL里面含有中文，所以需要用parse.quote进行处理，然后返回最终可调用的url
+    url = parse.quote("http://api.map.baidu.com" + queryStr + "&sn=" + sn, safe="/:=&?#+!$,;'@()*[]")
+
+    response = urllib.request.urlopen(url)
+    content = response.read().decode('utf-8')
+    result = eval(content)['result'].location
+    return result.lng, result.lat
+
+
 if __name__ == '__main__':
     get_address()
